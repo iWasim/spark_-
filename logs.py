cce83a22a633cce06ca109460e10b1294f7c4658
iWasim <76530148+iWasim@users.noreply.github.com>
2024-09-09 17:07:37 +0530

    Create logs.py

---
+++ logs.py
@@ -0,0 +1,18 @@
+CREATE OR REPLACE TABLE your_new_table_name AS
+SELECT 
+    object_id, 
+    view_name, 
+    CASE 
+        WHEN MAX(event_time) < DATE_SUB(current_date(), INTERVAL 1 YEAR) THEN '1y'
+        WHEN MAX(event_time) < DATE_SUB(current_date(), INTERVAL 6 MONTH) THEN '6m'
+    END AS flag
+FROM 
+    your_table_name
+GROUP BY 
+    object_id, 
+    view_name
+HAVING 
+    MAX(event_time) < DATE_SUB(current_date(), INTERVAL 6 MONTH)
+    OR MAX(event_time) < DATE_SUB(current_date(), INTERVAL 1 YEAR)
+
+=========================================================================
