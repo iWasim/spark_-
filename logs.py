c57857291650d09fc084bbae2c2559ba92be37e8
iWasim <76530148+iWasim@users.noreply.github.com>
2024-09-09 17:16:14 +0530

    Update logs.py

--- logs.py
+++ logs.py
@@ -16,3 +16,20 @@ HAVING
     OR MAX(event_time) < DATE_SUB(current_date(), INTERVAL 1 YEAR)
 
 =========================================================================
+
+CREATE OR REPLACE TABLE your_new_table_name AS
+SELECT 
+    object_id, 
+    view_name, 
+    CASE 
+        WHEN MAX(event_time) < DATE_SUB(current_date(), 365) THEN '1y'
+        WHEN MAX(event_time) < DATE_SUB(current_date(), 180) THEN '6m'
+    END AS flag
+FROM 
+    your_table_name
+GROUP BY 
+    object_id, 
+    view_name
+HAVING 
+    MAX(event_time) < DATE_SUB(current_date(), 180)
+    OR MAX(event_time) < DATE_SUB(current_date(), 365)
