a7f8ed2fd7bbafd16f1ad111f56e0eb64536ad25
iWasim <76530148+iWasim@users.noreply.github.com>
2024-09-23 11:59:49 +0530

    Update unused.sql

--- unused.sql
+++ unused.sql
@@ -32,3 +32,41 @@ FROM
 WHERE 
     a.event_time < NOW() - INTERVAL '1 year'
     OR (a.event_time < NOW() - INTERVAL '6 months' AND a.event_time >= NOW() - INTERVAL '1 year');
+
+
+
+==============
+
+SELECT 
+    s.object_id,
+    s.object_name,
+    s.event_time,
+    CASE 
+        WHEN DATEDIFF(NOW(), s.event_time) > 365 THEN '1y'
+        WHEN DATEDIFF(NOW(), s.event_time) > 180 THEN '6m'
+        ELSE NULL
+    END AS unused_dur,
+    's3' AS system
+FROM 
+    s3_table AS s
+WHERE 
+    DATEDIFF(NOW(), s.event_time) > 365
+    OR (DATEDIFF(NOW(), s.event_time) > 180 AND DATEDIFF(NOW(), s.event_time) <= 365)
+
+UNION ALL
+
+SELECT 
+    a.object_id,
+    a.object_name,
+    a.event_time,
+    CASE 
+        WHEN DATEDIFF(NOW(), a.event_time) > 365 THEN '1y'
+        WHEN DATEDIFF(NOW(), a.event_time) > 180 THEN '6m'
+        ELSE NULL
+    END AS unused_dur,
+    'abudp' AS system
+FROM 
+    abudp_table AS a
+WHERE 
+    DATEDIFF(NOW(), a.event_time) > 365
+    OR (DATEDIFF(NOW(), a.event_time) > 180 AND DATEDIFF(NOW(), a.event_time) <= 365);
